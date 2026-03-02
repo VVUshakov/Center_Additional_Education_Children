0dcb7aa5f8bc2920b4d5989051145cdfe8218783
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-02 16:26:55 +0700

    Update Frame.cs

--- Frame.cs
+++ Frame.cs
@@ -1,20 +1,17 @@
-﻿namespace ConsoleApp5
+// Класс для хранения состояния рамки игрового поля
+public class Frame
 {
-    // Класс для хранения состояния рамки игрового поля
-    public class Frame
+    public int Width { get; set; }  // Ширина игрового поля (внутренняя область)
+    public int Height { get; set; } // Высота игрового поля (внутренняя область)
+    
+    #region КОНСТРУКТОР
+    public Frame(
+        int width = 50, 
+        int height = 20
+    )
     {
-        public int Width { get; set; }                  // Ширина игрового поля (внутренняя область)
-        public int Height { get; set; }                 // Высота игрового поля (внутренняя область)
-        
-        #region КОНСТРУКТОР
-        public Frame(
-            int width = 50, 
-            int height = 20
-        )
-        {
-            Width = width;
-            Height = height;
-        }
-        #endregion
+        Width = width;
+        Height = height;
     }
+    #endregion
 }

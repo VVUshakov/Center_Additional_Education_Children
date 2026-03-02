04c66e194a9180f88bee86dbf776ec410db9419a
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-02 15:18:17 +0700

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    // Класс для хранения состояния еды на игровом поле
+    public class Food
+    {
+        public Point Position { get; set; } // Позиция еды на игровом поле
+        public bool IsActive { get; set; } // Существует ли еда на поле
+               
+        #region КОНСТРУКТОР
+        public Food(
+            Point position,            
+            bool isActive = true
+        )
+        {
+            Position = position;
+            IsActive = isActive;
+        }
+        #endregion
+    }
+}

387a7320f1c36b064757013aaedeac8485ca7ef9
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-02 16:19:34 +0700

    Update Snake.cs

--- Snake.cs
+++ Snake.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    internal class Snake
+    // Состояние змейки
+public class Snake
+{
+    public static List<Point> Body { get; set; }            // тело змейки: индекс 0 - хвост, последний индекс - голова
+    public static  Direction CurrentDirection { get; set; } // текущее направление движения
+    public static bool IsAlive { get; set; }                // флаг - жива ли змейка
+
+    #region КОНСТРУКТОР
+    public Snake(
+        List<Point> body,
+        Direction currentDirection = Direction.Right,
+        bool isAlive = true
+    )
     {
+        Body = body;
+        CurrentDirection = currentDirection;
+        IsAlive = isAlive;
     }
+    #endregion
 }
+}
+

c5b19af269c794262e1100b8a83fe953e4eced8a
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-02 15:01:49 +0700

    Add files via upload

---
+++ GameCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp5
+{
+    // Перечислитель команд
+    public enum GameCommand
+    {
+        None,   // нет
+        Up,     //вверх
+        Down,   // вниз
+        Left,   // влево
+        Right,  // вправо
+        Pause,  // пауза
+        Exit    // выход
+    }
+}

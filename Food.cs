f76b9b22f75b69d13048b014a73faef7b46f4e79
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-02 16:27:23 +0700

    Update Food.cs

--- Food.cs
+++ Food.cs
@@ -1,21 +1,17 @@
-namespace ConsoleApp5
+// Класс для хранения состояния еды на игровом поле
+public class Food
 {
-    // Класс для хранения состояния еды на игровом поле
-    public class Food
+    public Point Position { get; set; } // позиция еды на игровом поле
+    public bool IsActive { get; set; } // флаг - существует ли еда на поле
+           
+    #region КОНСТРУКТОР
+    public Food(
+        Point position = null,            
+        bool isActive = true
+    )
     {
-        public Point Position { get; set; } // позиция еды на игровом поле
-        public bool IsActive { get; set; } // флаг - существует ли еда на поле
-               
-        #region КОНСТРУКТОР
-        public Food(
-            Point position = null,            
-            bool isActive = true
-        )
-        {
-            Position = position ?? new Point(0, 0);
-            IsActive = isActive;
-        }
-        #endregion
+        Position = position ?? new Point(0, 0);
+        IsActive = isActive;
     }
+    #endregion
 }
-

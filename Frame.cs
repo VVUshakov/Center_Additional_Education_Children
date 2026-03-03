bdbed9462cda19adbc5976d116d8646049a5e680
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-04 01:01:25 +0700

    Update Frame.cs

--- Frame.cs
+++ Frame.cs
@@ -1,8 +1,10 @@
 // Класс для хранения состояния рамки игрового поля
 public class Frame
 {
-    public int Width { get; set; }  // Ширина игрового поля (внутренняя область)
-    public int Height { get; set; } // Высота игрового поля (внутренняя область)
+    public readonly int OFFSET = 5;   // сдвиг (для сервисных сообщений)
+    public int Width { get; set; }  // ширина игрового поля (внутренняя область)
+    public int Height { get; set; } // высота игрового поля (внутренняя область)
+
 
     #region КОНСТРУКТОР
     public Frame(

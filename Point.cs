852547d014842d0c9de9ee2aae23898a793ba92a
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-04 00:58:35 +0700

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -4,10 +4,11 @@ public class Point
     public int X { get; set; }
     public int Y { get; set; }
 
-    // Конструктор
+    #region КОНСТРУКТОР
     public Point(int x, int y)
     {
         X = x;
         Y = y;
     }
+    #endregion
 }

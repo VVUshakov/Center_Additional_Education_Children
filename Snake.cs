433ebc07ff75b5ac54769f35e03cdc61120c7282
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-04 00:55:58 +0700

    Update Snake.cs

--- Snake.cs
+++ Snake.cs
@@ -1,20 +1,70 @@
 // Состояние змейки
 public class Snake
 {
-    public static List<Point> Body { get; set; }            // тело змейки: индекс 0 - хвост, последний индекс - голова
-    public static Direction CurrentDirection { get; set; } // текущее направление движения
-    public static bool IsAlive { get; set; }                // флаг - жива ли змейка
+    public List<Point> Body { get; set; }           // тело змейки: индекс 0 - хвост, последний индекс - голова
+    public Direction CurrentDirection { get; set; } // текущее направление движения
+    public bool IsAlive { get; set; }               // флаг - жива ли змейка
+    public int Length { get; set; }                 // текущая длина змейки
 
     #region КОНСТРУКТОР
     public Snake(
-        List<Point> body,
-        Direction currentDirection = Direction.Right,
-        bool isAlive = true
+        Point head,                             // точка старта (голова)
+        Direction direction = Direction.Right,  // направление движения змейки
+        int snakeLength = 3,                    // длина змейки
+        bool isAlive = true                     // флаг жизни змейки
     )
     {
-        Body = body;
-        CurrentDirection = currentDirection;
+        CurrentDirection = Direction.Right;
+        Length = snakeLength;
+        Body = InitializeSnake(head, direction, snakeLength);
         IsAlive = isAlive;
     }
     #endregion
+
+    // Инициализировать тело новой змейки
+    private List<Point> InitializeSnake(Point head, Direction direction, int snakeLength = 3)
+    {
+        if(snakeLength < 1) snakeLength = 1; // длина змейки должна быть не меньше 1
+
+        var body = new List<Point>();
+
+        // Создаем сегменты от хвоста к голове
+        for(int i = snakeLength - 1; i >= 0; i--)
+        {
+            switch(direction)
+            {
+                case Direction.Right:
+                    // При движении вправо: хвост слева, голова справа
+                    body.Add(new Point(head.X - i, head.Y));
+                    break;
+
+                case Direction.Left:
+                    // При движении влево: хвост справа, голова слева
+                    body.Add(new Point(head.X + i, head.Y));
+                    break;
+
+                case Direction.Up:
+                    // При движении вверх: хвост снизу, голова сверху
+                    body.Add(new Point(head.X, head.Y + i));
+                    break;
+
+                case Direction.Down:
+                    // При движении вниз: хвост сверху, голова снизу
+                    body.Add(new Point(head.X, head.Y - i));
+                    break;
+
+                default:
+                    throw new ArgumentException($"Неизвестное направление: {direction}");
+            }
+        }
+
+        return body;
+    }
+
+    // Метод для увеличения длины змейки (при поедании еды)
+    public void Grow()
+    {
+        Length++;
+        // Логика добавления нового сегмента будет в другом методе
+    }
 }

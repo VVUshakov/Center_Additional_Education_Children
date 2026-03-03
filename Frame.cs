360cec0c8b66ac7090dc32ea1e8b23c131999009
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-03 15:53:46 +0700

    Update Frame.cs

--- Frame.cs
+++ Frame.cs
@@ -3,10 +3,10 @@ public class Frame
 {
     public int Width { get; set; }  // Ширина игрового поля (внутренняя область)
     public int Height { get; set; } // Высота игрового поля (внутренняя область)
-    
+
     #region КОНСТРУКТОР
     public Frame(
-        int width = 50, 
+        int width = 50,
         int height = 20
     )
     {

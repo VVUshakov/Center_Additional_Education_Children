b287947b4e8a8f6ac648e71a22673c6d95c1984b
VVUshakov <85858557+VVUshakov@users.noreply.github.com>
2026-03-03 17:09:09 +0700

    Add files via upload

---
+++ GameState.cs
@@ -0,0 +1,14 @@
+﻿// Класс для хранения состояния игры
+internal class GameState
+{
+    #region СОСТОЯНИЕ ИГРЫ
+    public static int _fieldWidth = 60;    // ширина игрового поля
+    public static int _fieldHeight = 30;   // высота игрового поля
+    public static int _fps = 100;          // пауза между циклами (миллисекунды)  
+    public static bool _isExit = false;    // флаг продолжения игры
+    public static bool _isPause = false;   // флаг паузы
+    public static Snake _snake;            // объект змейки
+    public static Frame _frame;            // объект игрового поля
+    public static Food _food;              // объект игрового поля
+    #endregion
+}
